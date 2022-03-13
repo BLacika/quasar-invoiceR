fd44401e3ffd2acb90b50f2a142519956f7df565
Laszlo Balassa <b.lacika23@gmail.com>
2022-03-13 22:32:16 +0100

    added store files

---
+++ src/store/bank.js
@@ -0,0 +1,15 @@
+import data from "./data.json";
+import { getCurrencyById } from "./currency"
+
+export function getBankAccounts() {
+  return data.bankAccounts.map(acc => getBankAccountById(acc.id));
+}
+
+export function getBankAccountById(id) {
+  const acc = data.bankAccounts.find(item => item.id === id);
+  // console.log('acc', acc)
+  return {
+    ...acc,
+    currencyId: getCurrencyById(acc.currencyId)
+  }
+}
